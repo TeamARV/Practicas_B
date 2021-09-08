99210f8481afb39d96135854ba3f5a4de10ca182
TeamARV <andresriveravega@msn.com>
2021-09-08 11:05:52 -0500

    updatetest

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("hola mundo")
+let variable="test"
+console.log(variable)
+
+let algo = prompt("ingrese numero","")
+let algo2 = prompt("ingrese numero2","")
+
+document.write("salida de una variable ")
+let algo3= algo*algo2
+document.write(algo3)
